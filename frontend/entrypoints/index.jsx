14c07686ec46513a4da88d3fad92c3a51c6f78e2
Min Hein <min.hein@carpetright.co.uk>
2024-07-16 16:33:25 +0100

    hellow

--- frontend/entrypoints/index.jsx
+++ frontend/entrypoints/index.jsx
@@ -15,7 +15,7 @@ import XotixSocial from '@components/XotixSocial';
 import Footer from '@components/Footer';
 
 const COMPONENTS = [
-  { $selector: '.cound-down__c', Component: Countdown },
+  { $selector: '.cound-down__c', Component: CountDown },
   { $selector: '.hello-world__c', Component: HelloWorld },
   { $selector: '.video-hero__cmp', Component: VideoHero },
   { $selector: '.featured-collection__cmp', Component: FeaturedCollection },
